f215f66f9941913349a0c5d5ac62d86a0c379008
Jair Olguin <emiliojairo@hotmail.com>
2023-04-23 16:32:26 -0600

    deleteComponent by ID

--- Routes/components.js
+++ Routes/components.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { getComponents } = require('../Controllers/getComponents.js');
 const { createComponents } = require('../Controllers/createComponents.js');
+const { deleteComponent } = require('../Controllers/deleteComponents.js');
 const components = express();
 
 components.get('/', async(req, res) => {
@@ -14,6 +15,14 @@ components.post('/', async(req, res) => {
     try {
         const {body} = req;
         res.status(201).send(await createComponents(body));
+    } catch (error) {
+        res.status(400).send({error});
+    }
+});
+components.delete('/delete/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        res.status(200).send(await deleteComponent(id));
     } catch (error) {
         res.status(400).send({error});
     }
